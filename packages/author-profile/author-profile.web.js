2cd72534fa91f234f10c3c1912d48c80f406d68f
Craig Bilner <craig.bilner@gmail.com>
2017-07-10 21:34:23 +0100

    fix: wire in data

--- packages/author-profile/author-profile.web.js
+++ packages/author-profile/author-profile.web.js
@@ -1,15 +1,44 @@
 import React from "react";
 import Markup from "@times-components/markup";
+import Card from "@times-components/card";
+
+const makeCard = ({
+  label,
+  title,
+  publishedTime,
+  publicationName,
+  teaser,
+  leadAsset
+}, i) => {
+  const uri = leadAsset.posterImage
+    ? leadAsset.posterImage.crop.url
+    : leadAsset.crop.url;
+  const props = {
+    key: `article-${i}`,
+    label,
+    headline: title,
+    date: publishedTime,
+    publication: publicationName,
+    text: teaser,
+    image: {
+      uri
+    }
+  };
+
+  return <Card {...props} />;
+};
 
 export default ({ data }) => {
   if (data.loading) {
     return <div>Loading . . .</div>;
   }
 
-  console.log(data.author);
-  debugger
+  const { biography, articles: { list } } = data.author;
 
-  const { biography } = data.author;
-
-  return <Markup ast={biography} />;
+  return (
+    <div>
+      <Markup ast={biography} />
+      {list.map(makeCard)}
+    </div>
+  );
 };
