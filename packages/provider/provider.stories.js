bec8a4de33f78efa756b84da258e3b0c339b1407
Filipe Pinheiro <me@fampinheiro.com>
2017-08-14 14:40:49 +0100

    chore: keep react consistent across package

--- packages/provider/provider.stories.js
+++ packages/provider/provider.stories.js
@@ -9,30 +9,18 @@ import {
   gql
 } from "react-apollo";
 
-import { withApolloProvider } from "storybook-addon-apollo-graphql";
-
 import connectGraphql, { AuthorProfileProvider } from "./provider.js";
 
 const Component = props => <Text>{JSON.stringify(props, null, 2)}</Text>;
 
-const schema = `
-    type Query {
-        random: Int!
-    }
-`;
+const query = gql`{
+  author(slug: "fiona-hamilton") { name }
+}`;
 
-const root = {
-  random: () => Math.floor(Math.random() * 10)
-};
-
-const query = gql`{ random }`;
-
-storiesOf("Provider", module)
-  .addDecorator(withApolloProvider({ schema, root }))
-  .add("Provider", () => {
-    const WithData = connectGraphql(query)(Component);
-    return <WithData />;
-  });
+storiesOf("Provider", module).add("Provider", () => {
+  const WithData = connectGraphql(query)(Component);
+  return <WithData />;
+});
 
 const fragmentMatcher = new IntrospectionFragmentMatcher({
   introspectionQueryResultData: {
