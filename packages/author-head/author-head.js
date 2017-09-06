48e595f5d544126c16a56eec11d37a96ed611545
Fabio Oliveira <fabio.an.oliveira@gmail.com>
2017-09-06 16:46:58 +0100

    fix: adds visual fixes to author head

--- packages/author-head/author-head.js
+++ packages/author-head/author-head.js
@@ -3,11 +3,20 @@ import { View, StyleSheet, Text } from "react-native";
 import PropTypes from "prop-types";
 
 import Image from "@times-components/image";
+import { renderTrees } from "@times-components/markup";
 import { TextLink } from "@times-components/link";
-import { renderTrees, treePropType } from "@times-components/markup";
+import withResponsiveStyle, {
+  Breakpoints
+} from "@times-components/responsive-hoc";
 
 const styles = StyleSheet.create({
+  wrapper: {
+    alignItems: "center",
+    backgroundColor: "transparent",
+    paddingBottom: 50
+  },
   container: {
+    display: "flex",
     width: "100%",
     alignItems: "center",
     flexDirection: "column",
@@ -17,8 +26,9 @@ const styles = StyleSheet.create({
   photoContainer: {
     width: 100,
     height: 100,
-    bottom: 0,
-    position: "absolute"
+    order: 1,
+    paddingTop: 16,
+    paddingBottom: 16
   },
   roundImage: {
     width: 100,
@@ -31,19 +41,22 @@ const styles = StyleSheet.create({
     fontFamily: "TimesModern-Bold",
     fontSize: 45,
     color: "#000",
-    paddingTop: 32
+    paddingTop: 32,
+    order: 2
   },
   title: {
     fontFamily: "TimesDigital-RegularSC",
     fontSize: 15,
-    color: "#696969"
+    color: "#696969",
+    order: 3
   },
   twitter: {
     fontSize: 18,
     fontFamily: "GillSansMTStd-Medium",
     color: "#006699",
     paddingTop: 16,
-    textDecorationLine: "none"
+    textDecorationLine: "none",
+    order: 4
   },
   bio: {
     fontFamily: "TimesDigital-Regular",
@@ -52,32 +65,61 @@ const styles = StyleSheet.create({
     lineHeight: 26,
     color: "#333",
     maxWidth: "88%",
-    paddingBottom: 32
-  },
-  wrapper: {
-    alignItems: "center",
-    backgroundColor: "transparent",
-    paddingBottom: 50
+    paddingBottom: 32,
+    order: 5
   }
 });
 
+const ResponsiveStyles = {
+  web: {
+    [Breakpoints.MEDIUM]: StyleSheet.create({
+      photoContainer: {
+        display: "none"
+      }
+    }),
+    [Breakpoints.LARGE]: StyleSheet.create({
+      photoContainer: {
+        order: 6,
+        paddingTop: 0,
+        paddingBottom: 0,
+        bottom: -50,
+        position: "absolute"
+      }
+    })
+  }
+};
+
 const AuthorHead = props => {
-  const { name, title, twitter, bio, uri } = props;
+  const { name, title, twitter, bio, uri, responsive } = props;
 
   return (
-    <View style={styles.wrapper} pointerEvents="box-none">
-      <View accessibilityRole="banner" style={styles.container}>
-        <Text accessibilityRole="heading" aria-level="1" style={styles.name}>
+    <View style={[styles.wrapper, responsive.wrapper]} pointerEvents="box-none">
+      <View
+        accessibilityRole="banner"
+        style={[styles.container, responsive.container]}
+      >
+        <Text
+          accessibilityRole="heading"
+          aria-level="1"
+          style={[styles.name, responsive.name]}
+        >
           {name}
         </Text>
-        <Text accessibilityRole="heading" aria-level="2" style={styles.title}>
+        <Text
+          accessibilityRole="heading"
+          aria-level="2"
+          style={[styles.title, responsive.title]}
+        >
           {title.toLowerCase()}
         </Text>
         <TwitterLink handle={twitter} />
         <Text style={styles.bio}>{renderTrees(bio)}</Text>
-      </View>
-      <View style={styles.photoContainer}>
-        <Image source={{ uri }} style={styles.roundImage} />
+        <View style={[styles.photoContainer, responsive.photoContainer]}>
+          <Image
+            source={{ uri }}
+            style={[styles.roundImage, responsive.roundImage]}
+          />
+        </View>
       </View>
     </View>
   );
@@ -95,7 +137,7 @@ AuthorHead.propTypes = {
   name: PropTypes.string,
   title: PropTypes.string,
   uri: PropTypes.string,
-  bio: PropTypes.arrayOf(treePropType),
+  bio: PropTypes.arrayOf(PropTypes.object),
   twitter: PropTypes.string
 };
 
@@ -120,4 +162,4 @@ TwitterLink.defaultProps = {
   handle: AuthorHead.defaultProps.twitter
 };
 
-export default AuthorHead;
+export default withResponsiveStyle(AuthorHead, ResponsiveStyles);
